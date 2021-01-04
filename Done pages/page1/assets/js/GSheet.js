ffeac5f1fe403dd2fdc5d37c2974fadc7abe939b
HelenStafford <67685601+HelenStafford@users.noreply.github.com>
2021-01-04 09:26:50 +0200

    2021-01-04

---
+++ Done pages/page1/assets/js/GSheet.js
@@ -0,0 +1,19 @@
+ const scriptURL = 'https://script.google.com/macros/s/AKfycbxPlDAUWksQ9hv4e1uYnvvTKcP9VNKUksP_jHlSGkAHfJhwqhkg/exec'
+const form = document.forms['google-sheet']
+
+var sbsuccess = false;
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {alert("Thanks for Contacting us..! We Will Contact You Soon..."); 
+      if(sbsuccess === false){
+        StartConfetti();
+        sbsuccess = true;
+      };
+      subscribe();
+      })
+    .catch(error => console.error('Error!', error.message))
+}) 
+
+
